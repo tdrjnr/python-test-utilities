b9b4a356ee32a6226b6a58283c491c3edf7f822a
Matthew D Jones <matthew.d.jones@tessella.com>
2017-02-10 11:58:05 +0000

    Improved comments on vagrant context manager

--- utils/cluster.py
+++ utils/cluster.py
@@ -4,21 +4,25 @@ from cd import cd
 
 class Cluster:
     """
-    Manage the virtual cluster
+    Context manager for Vagrant VMs
+    Will bring up the vagrant machines on entry and tear down on exit
     """
 
-    def __init__(self, repo_dir):
-        self.repo_dir = repo_dir
+    def __init__(self, script_dir):
+        """
+        :param script_dir: directory in which your vagrant script resides
+        """
+        self.script_dir = script_dir
 
     def __enter__(self):
-        if self.repo_dir:
-            with cd(self.repo_dir):
+        if self.script_dir:
+            with cd(self.script_dir):
                 v = vagrant.Vagrant(quiet_stdout=False)
                 v.up()
                 print("...virtual cluster is up.")
 
     def __exit__(self, etype, value, traceback):
-        if self.repo_dir:
-            with cd(self.repo_dir):
+        if self.script_dir:
+            with cd(self.script_dir):
                 v = vagrant.Vagrant(quiet_stdout=False)
                 v.halt()
