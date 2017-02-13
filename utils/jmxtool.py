674be99fbbbfdd5001bf178f74ba56a908df4c39
Matthew D Jones <matthew.d.jones@tessella.com>
2017-02-13 13:26:10 +0000

    Use absolute paths for JmxTool

--- utils/jmxtool.py
+++ utils/jmxtool.py
@@ -6,8 +6,6 @@ import csv
 import numpy as np
 import tarfile
 import urllib2
-import inspect
-from cd import cd
 
 
 class JmxTool:
@@ -26,67 +24,68 @@ class JmxTool:
         :param scala_version: specify Scala version
         :param interval_milliseconds: interval at which to get a value for the metric
         """
-        with cd(os.path.dirname(inspect.stack()[0][1])):
-            self._download_and_extract_kafka(kafka_version, scala_version)
+        utils_dir = os.path.dirname(os.path.abspath(__file__))
+        self._download_and_extract_kafka(utils_dir, kafka_version, scala_version)
 
-            mbean1 = "'kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec,topic=" + topic + "' "
-            mbean2 = "'kafka.server:type=BrokerTopicMetrics,name=BytesOutPerSec,topic=" + topic + "' "
-            attributes = "OneMinuteRate "
+        mbean1 = "'kafka.server:type=BrokerTopicMetrics,name=BytesInPerSec,topic=" + topic + "' "
+        mbean2 = "'kafka.server:type=BrokerTopicMetrics,name=BytesOutPerSec,topic=" + topic + "' "
+        attributes = "OneMinuteRate "
 
-            if metrics is "cpu":
-                mbean1 = "'java.lang:type=OperatingSystem' "
-                mbean2 = None
-                attributes = "ProcessCpuLoad "  # SystemCpuLoad
-            elif metrics is "memory":
-                mbean1 = "'java.lang:type=Memory' "
-                mbean2 = None
-                attributes = "HeapMemoryUsage "
+        if metrics is "cpu":
+            mbean1 = "'java.lang:type=OperatingSystem' "
+            mbean2 = None
+            attributes = "ProcessCpuLoad "  # SystemCpuLoad
+        elif metrics is "memory":
+            mbean1 = "'java.lang:type=Memory' "
+            mbean2 = None
+            attributes = "HeapMemoryUsage "
 
-            bin_dir = os.path.join("kafka_" + scala_version + "-" + kafka_version, "bin")
-            if self._is_windows():
-                run_script = "batch " + os.path.join(bin_dir, "windows", "kafka-run-class.bat")
-            else:
-                run_script = "bash " + os.path.join(bin_dir, "kafka-run-class.sh")
+        bin_dir = os.path.join(utils_dir,
+                               "kafka_" + scala_version + "-" + kafka_version, "bin")
+        if self._is_windows():
+            run_script = os.path.join(bin_dir, "windows", "kafka-run-class.bat")
+        else:
+            run_script = "bash " + os.path.join(bin_dir, "kafka-run-class.sh")
 
-            command = run_script + (" kafka.tools.JmxTool "
-                                    "--object-name " + mbean1)
-            if mbean2:
-                command = command + "--object-name " + mbean2
+        command = run_script + (" kafka.tools.JmxTool "
+                                "--object-name " + mbean1)
+        if mbean2:
+            command = command + "--object-name " + mbean2
 
-            command = (command + "--jmx-url "
-                                 "service:jmx:rmi:///jndi/rmi://" + host +
-                       "/jmxrmi "
-                       "--attributes " + attributes +
-                       "--reporting-interval " + str(interval_milliseconds))
+        command = (command + "--jmx-url "
+                             "service:jmx:rmi:///jndi/rmi://" + host +
+                   "/jmxrmi "
+                   "--attributes " + attributes +
+                   "--reporting-interval " + str(interval_milliseconds))
 
-            if self._is_windows():
-                self.jmxtool = pexpect.popen_spawn.PopenSpawn(command)
-            else:
-                self.jmxtool = pexpect.spawn(command)
+        if self._is_windows():
+            self.jmxtool = pexpect.popen_spawn.PopenSpawn(command)
+        else:
+            self.jmxtool = pexpect.spawn(command)
 
-            self.host = host
-            self.attributes = attributes.strip()
+        self.host = host
+        self.attributes = attributes.strip()
 
-    def _download_and_extract_kafka(self, kafka_version, scala_version):
-        kafka_dir_name = 'kafka_' + scala_version + '-' + kafka_version
-        kafka_file_name = kafka_dir_name + '.tgz'
+    def _download_and_extract_kafka(self, extract_to_dir, kafka_version, scala_version):
+        file_no_ext = 'kafka_' + scala_version + '-' + kafka_version
+        kafka_dir_name = os.path.join(extract_to_dir, file_no_ext)
+        kafka_file_name = os.path.join(extract_to_dir, file_no_ext) + '.tgz'
         if not os.path.isfile(kafka_file_name):
-            url = 'http://mirror.ox.ac.uk/sites/rsync.apache.org/kafka/' + kafka_version + '/' + kafka_file_name
-            self._download_file(url)
-            if not os.path.isdir('./' + kafka_dir_name):
-                tar = tarfile.open(kafka_file_name)
-                tar.extractall()
-                tar.close()
+            url = 'http://mirror.ox.ac.uk/sites/rsync.apache.org/kafka/' + kafka_version + '/' + file_no_ext + '.tgz'
+            self._download_file(url, kafka_file_name)
+        if not os.path.isdir(kafka_dir_name):
+            tar = tarfile.open(kafka_file_name)
+            tar.extractall(path=extract_to_dir)
+            tar.close()
 
     @staticmethod
-    def _download_file(url):
-        file_name = url.split('/')[-1]
+    def _download_file(url, filename):
         req = urllib2.Request(url, headers={'User-Agent': "Magic Browser"})
         u = urllib2.urlopen(req)
-        f = open(file_name, 'wb')
+        f = open(filename, 'wb')
         meta = u.info()
         file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading Apache Kafka: %s Bytes: %s" % (file_name, file_size)
+        print "Downloading Apache Kafka: %s Bytes: %s" % (filename, file_size)
 
         file_size_dl = 0
         block_sz = 8192
